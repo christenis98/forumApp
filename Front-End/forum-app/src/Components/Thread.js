d46d4cfd35b5f080fd2cd6dda43bc834289f9960
christenis98 <canei01@ucm.es>
2022-12-02 09:15:14 +0100

    Update Thread.js

--- Front-End/forum-app/src/Components/Thread.js
+++ Front-End/forum-app/src/Components/Thread.js
@@ -2,12 +2,18 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 const Thread = () => {
+  const handleClick = () => {
+    console.log("hola");
+  };
+
   return (
     <Card>
       <Card.Header>Thread Name</Card.Header>
       <Card.Body>
         <Card.Text>Thread description</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button onClick={handleClick} variant="primary">
+          New Post
+        </Button>
       </Card.Body>
     </Card>
   );
